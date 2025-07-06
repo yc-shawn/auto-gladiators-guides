9f6b09605a29d2a84622566c8a015a3c9ba2e755
Shawn Chen <ethanloatrchen@gmail.com>
2025-07-05 18:59:34 -0700

    update images

--- src/guides/heroes.ts
+++ src/guides/heroes.ts
@@ -55,7 +55,7 @@ export const heroes: Hero[] = [
     name: '剑姬',
     rank: Rank.A,
     sects: [FURY],
-    image: require('./images/heroes/jianji.png'),
+    image: require('./images/heroes/jianji.jpeg'),
     guides: [
       { sects: [ULTI, FURY], description: '右右右' },
       { sects: [ATTACK, FURY], description: '左左左+晶' },
@@ -65,7 +65,7 @@ export const heroes: Hero[] = [
     name: '魔尊',
     rank: Rank.B,
     sects: [CHAOS],
-    image: require('./images/heroes/mozun.png'),
+    image: require('./images/heroes/mozun.jpeg'),
     guides: [
       { sects: [HEALTH, CHAOS], description: '右右右+晶' },
       { sects: [HEALTH, CHAOS], description: '右左右' },
@@ -75,7 +75,7 @@ export const heroes: Hero[] = [
     name: '锤妹',
     rank: Rank.B,
     sects: [SHIELD],
-    image: require('./images/heroes/chuimei.png'),
+    image: require('./images/heroes/chuimei.jpeg'),
     guides: [
       { sects: [ULTI, SHIELD], description: '左左右+晶' },
       { sects: [CRIT, SHIELD], description: '左左右' },
@@ -85,14 +85,14 @@ export const heroes: Hero[] = [
     name: '和尚',
     rank: Rank.B,
     sects: [ATTACK],
-    image: require('./images/heroes/heshang.png'),
+    image: require('./images/heroes/heshang.jpeg'),
     guides: [{ sects: [ULTI, ATTACK], description: '右右左' }],
   },
   {
     name: '电音',
     rank: Rank.B,
     sects: [NONE],
-    image: require('./images/heroes/dianyin.png'),
+    image: require('./images/heroes/dianyin.jpeg'),
     guides: [
       { sects: [ULTI, REGEN], description: '左左左' },
       { sects: [ULTI, REGEN], description: '左左右' },
